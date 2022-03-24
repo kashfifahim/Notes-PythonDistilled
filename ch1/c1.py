ed4f9ea042b32b6681254422ba72766932c725c3
Kashfi <kashfi.fahim74@qmail.cuny.edu>
2022-03-24 17:26:02 -0400

    practice

---
+++ ch1/c1.py
@@ -0,0 +1,9 @@
+a = 'Hello World!'
+b = "Python is groovy"
+c = """Computer still says no"""
+
+print('''Content-type: text/html
+
+<h1>Hello World</h1>
+Click <a href="http://www.python.org">here</a>
+''')
